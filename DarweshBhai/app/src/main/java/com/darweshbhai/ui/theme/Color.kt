4a1b63cdfab4c97fbcc67a13bce613ec1be9fb73
MgagaCode <mgaga1949@gmail.com>
2025-03-15 11:21:19 +0500

    Add DarweshBhai/app/src/main/java/com/darweshbhai/ui/theme/Color.kt 1742019679826

---
+++ DarweshBhai/app/src/main/java/com/darweshbhai/ui/theme/Color.kt
@@ -0,0 +1,45 @@
+package com.darweshbhai.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Dark Theme Colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+// Custom App Colors
+val PrimaryBlue = Color(0xFF2196F3)
+val SecondaryBlue = Color(0xFF1976D2)
+val AccentOrange = Color(0xFFFF5722)
+val BackgroundLight = Color(0xFFF5F5F5)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF1E1E1E)
+val TextPrimaryLight = Color(0xFF000000)
+val TextPrimaryDark = Color(0xFFFFFFFF)
+val TextSecondaryLight = Color(0xFF757575)
+val TextSecondaryDark = Color(0xFFB3B3B3)
+val SuccessGreen = Color(0xFF4CAF50)
+val ErrorRed = Color(0xFFF44336)
+val WarningYellow = Color(0xFFFFC107)
+val InfoBlue = Color(0xFF2196F3)
+
+// Focus Mode Colors
+val FocusRed = Color(0xFFE57373)
+val FocusGreen = Color(0xFF81C784)
+val FocusYellow = Color(0xFFFFD54F)
+
+// Productivity Colors
+val TaskHighPriority = Color(0xFFF44336)
+val TaskMediumPriority = Color(0xFFFFA726)
+val TaskLowPriority = Color(0xFF66BB6A)
+
+// Usage Stats Colors
+val UsageHigh = Color(0xFFE57373)
+val UsageMedium = Color(0xFFFFB74D)
+val UsageLow = Color(0xFF81C784)
